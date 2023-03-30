bb728cf197b90b0fef036c3538416041eca2e95c
AlexGuSpb <spbalexgus@yandex.ru>
2023-03-30 20:42:48 +0300

    12.0. Написать тесты на StartUI. [#785]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -6,49 +6,46 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StartUITest {
     @Test
     public void whenCreateItem() {
-        Output output = new ConsoleOutput();
         Input in = new StubInput(
-                new String[]{"0", "Item name", "1"}
+                new String[] {"0", "Item name", "1"}
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction(output),
+                new CreateAction(),
                 new ExitAction()
         };
-        new StartUI(output).init(in, tracker, actions);
+        new StartUI().init(in, tracker, actions);
         assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
     }
 
     @Test
-    public void whenEditItem() {
-        Output output = new ConsoleOutput();
+    public void whenReplaceItem() {
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Replaced item"));
         String replacedName = "New item name";
         Input in = new StubInput(
-        new String[]{"0", String.valueOf(item.getId()), "New item name", "1"}
+                new String[] {"0", String.valueOf(item.getId()), replacedName, "1"}
         );
         UserAction[] actions = {
-                new EditAction(output),
+                new EditAction(),
                 new ExitAction()
         };
-        new StartUI(output).init(in, tracker, actions);
+        new StartUI().init(in, tracker, actions);
         assertThat(tracker.findById(item.getId()).getName()).isEqualTo(replacedName);
     }
 
     @Test
     public void whenDeleteItem() {
-        Output output = new ConsoleOutput();
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Deleted item"));
         Input in = new StubInput(
-        new String[]{"0", String.valueOf(item.getId()), "1"}
+                new String[] {"0", String.valueOf(item.getId()), "1"}
         );
         UserAction[] actions = {
-                new DeleteAction(output),
+                new DeleteAction(),
                 new ExitAction()
         };
-        new StartUI(output).init(in, tracker, actions);
+        new StartUI().init(in, tracker, actions);
         assertThat(tracker.findById(item.getId())).isNull();
     }
 }
