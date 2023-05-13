21196f8151b28840851c8b464dae0ac3f85c399f
AlexGuSpb <spbalexgus@yandex.ru>
2023-05-13 12:40:02 +0300

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,8 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("spbalexgus@yandex.ru", "Gusev Alexey Vyacheslavovich");
+        map.put("spbalexgus@yandex.ru", "Gusev Alexey Vyacheslavovich");
+        map.put("spbalexgus@yandex.com", "Gus Alexey Vyacheslavovich");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
